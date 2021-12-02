84936904b301d88a2015083b812c5ab9379e6fea
Antonio Sucre <antonio.sucre@indikatore.com>
2021-12-02 16:18:47 -0300

    initializing-database

--- App.js
+++ App.js
@@ -6,6 +6,13 @@ import ReduxThunk from 'redux-thunk';
 
 import PlacesNavigator from './navigation/PlacesNavigator';
 import placesReducer from './store/places-reducer';
+import { init } from './helpers/db';
+
+init().then(() => {
+  console.log('Initialized database');
+}).catch(err => {
+  console.log('Initializing db failed')
+});
 
 const rootReducer = combineReducers({
   places: placesReducer
